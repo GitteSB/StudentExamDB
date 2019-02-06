9dd4819bc25815ffa0f3b7b2a7a6da8498ecbf41
Martin Kierkegaard <martinkierkegaard@yahoo.com>
2019-02-06 12:03:25 +0100

    Eksempel på en Stored procedure som indsætter en række i tabellen Student, som returnerer antal rækker osm er indsat i tabellen

---
+++ StoredProcedure_Insert.sql
@@ -0,0 +1,19 @@
+﻿
+/*
+Eksempel på storedprocedure som indsætter en række
+i student tabellen.
+Proceduren tager imod to parametre, @Name som er navnet på den studerende
+og @MobileNr som er tlfnr.
+proceduren returnerer værdien antal rækker, som er indsat i tabellen
+*/
+CREATE PROCEDURE AddPerson
+@Name NVARCHAR(30),
+@MobilNr NVARCHAR(12)
+AS
+INSERT INTO [dbo].[Student] ([Name], [Mobilnr]) 
+VALUES (@Name, @MobilNr)
+RETURN @@ROWCOUNT
+--Udfør proceduren
+EXECUTE dbo.AddPerson @Name = N'Kurt',   -- nvarchar(30)
+                      @MobilNr = N'23421' -- nvarchar(12)
+
