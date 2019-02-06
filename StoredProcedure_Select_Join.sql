1b018114db7970c2a1229c8b26829ac95685a389
Martin Kierkegaard <martinkierkegaard@yahoo.com>
2019-02-06 11:33:31 +0100

    Eksempler på en stored procedure med og uden parameter

---
+++ StoredProcedure_Select_Join.sql
@@ -0,0 +1,34 @@
+﻿/*Eksempel på en stored procedure som
+  er en join mellem de to tabeller Exam og Student.
+  Der retureners name, studentid og grade
+*/
+DROP PROCEDURE GetAllStudentGrade
+GO
+DROP PROCEDURE GetSpecificStudentGrade
+GO
+CREATE PROCEDURE GetAllStudentGrade AS
+   SELECT s.Name, s.studentid, e.grade
+   FROM student  s
+   INNER JOIN exam e ON s.StudentId = e.StudentId  
+  GO
+  --Udfør proceduren dbo.GetAllStudentGrade
+  EXEC dbo.GetAllStudentGrade
+  GO
+ 
+ /*Eksempel på en stored procedure som
+  er en join mellem de to tabeller Exam og Student, 
+  hvor der er defineret en parameter @StudentId, som bruges
+  I Where sætningen.
+  Der returneres name, studentid og grade
+*/
+ CREATE PROCEDURE GetSpecificStudentGrade
+   @StudentId INT
+  AS
+   SELECT s.Name, s.studentid, e.grade
+   FROM student  s
+   INNER JOIN exam e ON s.StudentId = e.StudentId  
+    WHERE s.StudentId = @StudentId
+  GO
+  --udfør proceduren GetSpecificStudentGrade med paramteren @StudentId sat til 230
+  EXEC dbo.GetSpecificStudentGrade @StudentId = 230 -- int
+  
